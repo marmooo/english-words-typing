48d9557bb24d9a8219cf4d8b9563042047b873ee
marmooo <marmooo@users.noreply.github.com>
2021-10-08 00:34:00 +0900

    bump bootstrap from 5.1.1 to 5.1.2

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-09-23 15:35';
+var CACHE_NAME = '2021-10-08 00:19';
 var urlsToCache = [
   '/english-words-typing/',
   '/english-words-typing/index.js',
@@ -10,7 +10,7 @@ var urlsToCache = [
   '/english-words-typing/keyboard.mp3',
   '/english-words-typing/favicon/original.svg',
   'https://marmooo.github.io/fonts/textar-light.woff2',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/simple-keyboard@latest/build/index.js',
   'https://cdn.jsdelivr.net/npm/simple-keyboard@latest/build/css/index.css',
 ];
