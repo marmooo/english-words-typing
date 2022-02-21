b91db6a502e422ee0e73393001e06dcb1849e3dc
marmooo <marmooo@users.noreply.github.com>
2022-02-22 08:39:38 +0900

    fix resizing of AA

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-02-13 10:20';
+var CACHE_NAME = '2022-02-22 08:40';
 var urlsToCache = [
   '/english-words-typing/',
   '/english-words-typing/index.js',
@@ -11,8 +11,7 @@ var urlsToCache = [
   '/english-words-typing/favicon/original.svg',
   'https://marmooo.github.io/fonts/textar-light.woff2',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/simple-keyboard@latest/build/index.js',
-  'https://cdn.jsdelivr.net/npm/simple-keyboard@latest/build/css/index.css',
+  "https://cdn.jsdelivr.net/npm/simple-keyboard@3.4.52/build/index.min.js",
 ];
 
 self.addEventListener('install', function(event) {
