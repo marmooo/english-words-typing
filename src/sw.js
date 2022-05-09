eae060d175acc13f8cb185c7e34629ac2106489c
marmooo <marmooo@users.noreply.github.com>
2022-05-09 09:03:50 +0900

    add OGP

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-04-15 09:25';
+var CACHE_NAME = '2022-05-09 09:05';
 var urlsToCache = [
   '/english-words-typing/',
   '/english-words-typing/index.js',
@@ -7,7 +7,7 @@ var urlsToCache = [
   '/english-words-typing/mp3/correct.mp3',
   '/english-words-typing/mp3/end.mp3',
   '/english-words-typing/mp3/keyboard.mp3',
-  '/english-words-typing/favicon/original.svg',
+  '/english-words-typing/favicon/favicon.svg',
   'https://marmooo.github.io/fonts/textar-light.woff2',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
   "https://cdn.jsdelivr.net/npm/simple-keyboard@3.4.52/build/index.min.js",
