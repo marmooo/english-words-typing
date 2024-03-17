0ed1cb85a24f3dc06762b6ad22a3801e2020292e
marmooo <marmooo@users.noreply.github.com>
2024-03-17 15:37:41 +0900

    use globalThis

--- src/index.js
+++ src/index.js
@@ -111,7 +111,7 @@ const simpleKeyboard = new SimpleKeyboard.default({
     }
   },
 });
-const audioContext = new AudioContext();
+const audioContext = new globalThis.AudioContext();
 const audioBufferCache = {};
 loadAudio("end", "mp3/end.mp3");
 loadAudio("keyboard", "mp3/keyboard.mp3");
@@ -269,7 +269,7 @@ function loadVoices() {
 
 function loopVoice(text, n) {
   speechSynthesis.cancel();
-  const msg = new SpeechSynthesisUtterance(text);
+  const msg = new globalThis.SpeechSynthesisUtterance(text);
   msg.voice = englishVoices[Math.floor(Math.random() * englishVoices.length)];
   msg.lang = "en-US";
   for (let i = 0; i < n; i++) {
@@ -553,7 +553,7 @@ function countdown() {
       infoPanel.classList.remove("d-none");
       scorePanel.classList.add("d-none");
       resizeFontSize(aa);
-      window.scrollTo({
+      globalThis.scrollTo({
         top: document.getElementById("typePanel").getBoundingClientRect().top,
         behavior: "auto",
       });
@@ -631,7 +631,7 @@ resizeFontSize(aa);
 document.getElementById("toggleDarkMode").onclick = toggleDarkMode;
 document.getElementById("toggleBGM").onclick = toggleBGM;
 document.getElementById("virtualKeyboard").onclick = toggleKeyboard;
-window.addEventListener("resize", () => {
+globalThis.addEventListener("resize", () => {
   resizeFontSize(aa);
 });
 document.getElementById("mode").onclick = changeMode;
