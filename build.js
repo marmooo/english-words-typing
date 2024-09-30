f75fed662bf267fedcb5bc937c401e2e9dcd0424
marmooo <marmooo@users.noreply.github.com>
2024-09-30 09:44:19 +0900

    use TextLineStream

--- build.js
+++ build.js
@@ -1,9 +1,12 @@
-import { readLines } from "https://deno.land/std/io/mod.ts";
+import { TextLineStream } from "jsr:@std/streams/text-line-stream";
 
 const range = [0, 400, 600, 800, 1200, 1600, 2200, 3000, 5000];
 const mGSL = [];
-const fileReader = await Deno.open("mGSL/dist/mGSL.lst");
-for await (const line of readLines(fileReader)) {
+const file = await Deno.open("mGSL/dist/mGSL.lst");
+const lineStream = file.readable
+  .pipeThrough(new TextDecoderStream())
+  .pipeThrough(new TextLineStream());
+for await (const line of lineStream) {
   mGSL.push(line);
 }
 for (let i = 1; i < range.length; i++) {
