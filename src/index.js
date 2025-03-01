ef5bbfab0d9121911caffc8f71dc35aebe44e929
marmooo <marmooo@users.noreply.github.com>
2025-03-01 23:44:45 +0900

    improve UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import simpleKeyboard from "https://cdn.jsdelivr.net/npm/simple-keyboard@3.7.77/+esm";
 
-document.getElementById("gamePanel")
-  .appendChild(document.getElementById("gamePanelTemplate").content);
-
 const remSize = parseInt(getComputedStyle(document.documentElement).fontSize);
 const gamePanel = document.getElementById("gamePanel");
 const infoPanel = document.getElementById("infoPanel");
