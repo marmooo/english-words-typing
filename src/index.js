1b80ebd2374e55936b538f066efade3cdf447e3b
marmooo <marmooo@users.noreply.github.com>
2025-06-28 15:11:23 +0900

    improve UI

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import simpleKeyboard from "https://cdn.jsdelivr.net/npm/simple-keyboard@3.7.77/+esm";
 
-document.getElementById("gamePanel")
-  .appendChild(document.getElementById("gamePanelTemplate").content);
-
 const remSize = parseInt(getComputedStyle(document.documentElement).fontSize);
 const gamePanel = document.getElementById("gamePanel");
+const keyboardPanel = document.getElementById("keyboard");
 const infoPanel = document.getElementById("infoPanel");
 const countPanel = document.getElementById("countPanel");
 const scorePanel = document.getElementById("scorePanel");
@@ -542,6 +540,7 @@ function countdown() {
   document.getElementById("guideSwitch").disabled = true;
   document.getElementById("virtualKeyboard").disabled = true;
   gamePanel.classList.add("d-none");
+  keyboardPanel.classList.add("d-none");
   infoPanel.classList.add("d-none");
   countPanel.classList.remove("d-none");
   counter.textContent = 3;
@@ -562,14 +561,11 @@ function countdown() {
       document.getElementById("guideSwitch").disabled = false;
       document.getElementById("virtualKeyboard").disabled = false;
       gamePanel.classList.remove("d-none");
+      keyboardPanel.classList.remove("d-none");
       countPanel.classList.add("d-none");
       infoPanel.classList.remove("d-none");
       scorePanel.classList.add("d-none");
       resizeFontSize(aa);
-      globalThis.scrollTo({
-        top: document.getElementById("gamePanel").getBoundingClientRect().top,
-        behavior: "auto",
-      });
       typable();
       startTypeTimer();
     }
@@ -615,6 +611,7 @@ function scoring() {
   playing = false;
   infoPanel.classList.remove("d-none");
   gamePanel.classList.add("d-none");
+  keyboardPanel.classList.add("d-none");
   countPanel.classList.add("d-none");
   scorePanel.classList.remove("d-none");
   const grade = gradeOption.options[gradeOption.selectedIndex].value;
