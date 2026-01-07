bcb1c8c2343efec897a75a0d4483f75cff882475
marmooo <marmooo@users.noreply.github.com>
2026-01-08 07:29:06 +0900

    update dict

--- build-dict.js
+++ build-dict.js
@@ -2,12 +2,12 @@ import { TextLineStream } from "jsr:@std/streams/text-line-stream";
 
 const range = [0, 400, 600, 800, 1200, 1600, 2200, 3000, 5000];
 const mGSL = [];
-const file = await Deno.open("mGSL/dist/mGSL.lst");
+const file = await Deno.open("mGSL/dist/mGSL.csv");
 const lineStream = file.readable
   .pipeThrough(new TextDecoderStream())
   .pipeThrough(new TextLineStream());
 for await (const line of lineStream) {
-  mGSL.push(line);
+  mGSL.push(line.replace(",", "\t"));
 }
 for (let i = 1; i < range.length; i++) {
   const outPath = "src/data/" + (i + 6) + ".tsv";
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "2025-12-18 00:00";
+const cacheName = "2026-01-08 00:00";
 const urlsToCache = [
   "/english-words-typing/index.js",
   "/english-words-typing/mp3/bgm.mp3",
